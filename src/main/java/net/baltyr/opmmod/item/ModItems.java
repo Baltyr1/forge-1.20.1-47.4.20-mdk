4e68c563fe606c9b452fce28f6589b5a8de73c5b
Baltyr1 <dufonteniouxarthur@gmail.com>
2026-04-20 17:53:29 +0200

    add du premier item

--- src/main/java/net/baltyr/opmmod/item/ModItems.java
+++ src/main/java/net/baltyr/opmmod/item/ModItems.java
@@ -2,10 +2,18 @@ package net.baltyr.opmmod.item;
 
 import net.baltyr.opmmod.OpmMod;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, OpmMod.MOD_ID);
+
+    public static final RegistryObject<Item> YEN = ITEMS.register("yen", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
